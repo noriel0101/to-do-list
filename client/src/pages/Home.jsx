6490802936e407620c9fdf1b4083a28330326098
Jobelyn01 <jobelynbritos1994@gmail.com>
2026-02-17 21:36:08 +0800

    Fixed folder structure and api paths

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "./api";
+import api from "../api";
 
 function Home() {
   const [lists, setLists] = useState([]);
--- client/src/pages/ListItem.jsx
+++ client/src/pages/ListItem.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "./api";
+import api from "../api";
 
 function ListItem() {
   const { listId } = useParams();
